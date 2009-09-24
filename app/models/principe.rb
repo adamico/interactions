8258b75cded570a02c63aca730049a830aa2185c
Andrea <andrea.damico@gmail.com>
2009-09-24 13:51:15 +0200

    added new and create actions and templates

--- app/models/principe.rb
+++ app/models/principe.rb
@@ -1,4 +1,6 @@
 class Principe < ActiveRecord::Base
+  validates_presence_of :name, :on => :create, :message => "name can't be blank"
+
   has_and_belongs_to_many :specialites
   has_and_belongs_to_many :classes
 end
